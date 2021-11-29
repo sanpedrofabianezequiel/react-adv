875ed26bb04777308af32f77003e65880ac9258d
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2021-11-28 22:19:41 -0300

    Update

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -12,9 +12,9 @@ interface Route {
 }
 
 
-const LazyPage1 = lazy(()=> import('../01-lazyload/pages/LazyPage1'));
-const LazyPage2 = lazy(()=> import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy(()=> import('../01-lazyload/pages/LazyPage3'));
+const LazyPage1 = lazy(()=> import(/* webpackChunkName: "LazyPage1"*/'../01-lazyload/pages/LazyPage1'));
+const LazyPage2 = lazy(()=> import(/* webpackChunkName: "LazyPage2"*/'../01-lazyload/pages/LazyPage2'));
+const LazyPage3 = lazy(()=> import(/* webpackChunkName: "LazyPage3"*/'../01-lazyload/pages/LazyPage3'));
 
 export const routes : Route[]=[
     {
