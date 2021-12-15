63eb3247923c4b529ec5511fd686f72f8e736931
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2021-12-15 16:22:07 -0300

    Pre lunch

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -18,26 +18,18 @@ export const ShoppingPage = () => {
             <ProductCard 
                 key={ product.id }
                 product={ product }
-                className="bg-dark text-white"
                 initialValues={{
                     count: 6,
-                    // maxCount: 10,
+                    maxCount: 10,
                 }}
             >
                 {
                     ({ reset, count, isMaxCountReached, maxCount, increaseBy  }) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-                            
-                            <button onClick={ reset }>Reset</button>
-                            <button onClick={ () => increaseBy(-2) }> -2 </button>
-                            {
-                                ( !isMaxCountReached && <button onClick={ ()=> increaseBy(+2) }> +2 </button> )
-                            }
-                            
-                            <span>{ count } - { maxCount }</span>
+                            <ProductImage/>
+                            <ProductTitle />
+                            <ProductButtons />
+                         
                         </>
                     )
                 }
